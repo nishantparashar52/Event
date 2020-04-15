5633190a4634492d619a1c31f8b898698b3d166c
Nishant <nishant.parashar@lenskart.in>
2020-04-15 10:39:07 +0530

    Adding changes for Artist Cards

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Search from './Search/Search';
+import Cards from './Cards/Cards';
 
 const Artist = () => {
   return (
     <div>
       <Search />
+      <div style={{marginTop: '20px'}}>
+        <Cards />
+      </div>
     </div>
   );
 };
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Cards = () => {
+    return (
+        <div className="row">
+            <div className="card col-lg-3">
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div className="card col-lg-3">
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div className="card col-lg-3">
+                <img className="card-img-top" src="..." alt="Card image cap" />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+
+    )
+};
+export default Cards;
