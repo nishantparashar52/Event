065ebee339c62d5313bd6dc5913a9e0653c21973
Nishant <nishant.parashar@lenskart.in>
2020-04-23 01:39:39 +0530

    bundled build

---
+++ build/precache-manifest.f41734c267ac9d6f7684f6dfa826d7d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb67a4c4d27805ee003ed1bc4f42a50b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eea58c3414ab5ee02e4",
+    "url": "/static/css/main.64469c6f.chunk.css"
+  },
+  {
+    "revision": "e9eb85580ec1d487449d",
+    "url": "/static/js/2.daa733a9.chunk.js"
+  },
+  {
+    "revision": "6cc4b8a594d2d83d5b536e42a9e88a21",
+    "url": "/static/js/2.daa733a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eea58c3414ab5ee02e4",
+    "url": "/static/js/main.962b86cf.chunk.js"
+  },
+  {
+    "revision": "0a31077b63893c09ca27",
+    "url": "/static/js/runtime-main.dbea746c.js"
+  }
+]);
